6b0f1788d947a335de12cf3a2406578b833003b8
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-08 20:52:44 +0900

    for문설명과 예시

---
+++ HD_pss_210408_1/HD_pss_210408_1/Source.cpp
@@ -0,0 +1,48 @@
+/*
+for([초기화 되는 변수 즉 , 시작지점 ]; [조건] 반복횟수; 시작변수를 증감 시킴 +- ; ){
+
+
+}
+
+*/
+
+#include<stdio.h>
+
+void main() {
+	
+	
+	
+	for (int i = 10; i >0; i++) {
+
+		//i=10
+		printf("hello world?\n");
+		//i=11
+
+	}
+
+	fgetc(stdin);
+
+	
+	
+	
+	/*while (true) {
+
+	}
+
+	int numberA;
+
+	int arrayB[10];
+	
+	numberA = 10;
+
+	for (int i = 0; i < sizeof(arrayB); i++) {
+		arrayB[i] = 0;
+
+
+		}
+*/
+
+
+
+
+}
